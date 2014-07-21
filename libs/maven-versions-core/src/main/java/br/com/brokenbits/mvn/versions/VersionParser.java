a9b7bb264e09495411b4f20652a0989ee10de349
Fabio Jun Takada Chino <fjtc@users.noreply.github.com>
2014-07-21 17:48:20 -0300

    VersionParser implementation and unit-tests completed.

--- libs/maven-versions-core/src/main/java/br/com/brokenbits/mvn/versions/VersionParser.java
+++ libs/maven-versions-core/src/main/java/br/com/brokenbits/mvn/versions/VersionParser.java
@@ -1,130 +1,168 @@
 package br.com.brokenbits.mvn.versions;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ * This class implements a maven version parser. This parser can operate in 2 modes,
+ * the strict mode and lazy mode.
+ * 
+ * <p>On strict mode, only versions in the form "x.y[.z][-qualifier][-b]". On lazy mode,
+ * only versions in the form "x.y[.z[.b]]" are accepted. 
+ * 
+ * @author FJTC
+ * @since 2014.07.21
+ */
 public class VersionParser {
 	
-	private boolean strict;
+	/**
+	 * A pattern designed to accept versions with the format "x.y[.z[.b]]".
+	 */
+	private static final Pattern MAJOR_MINOR_REVISION_BUILD_PATTERN = Pattern.compile("^([0-9]+)\\.([0-9]+)(?:\\.([0-9]+))?(?:\\.([0-9]+))?$");
 	
+	/**
+	 * A pattern designed to accept versions with the format "b".
+	 */
+	private static final Pattern BUILD_PATTERN = Pattern.compile("^([0-9]+)$");
+	
+	/**
+	 * A pattern designed to accept versions with the format "[qualifier][-b]".
+	 */
+	private static final Pattern QUALIFIER_BUILD_PATTERN = Pattern.compile("^([^\\-]+)?(?:\\-([0-9]+))?$");
+	
+	/**
+	 * A flag that indicates if this parser is strict or lazy.
+	 */
+	private boolean strict;	
+	
+	/**
+	 * Creates a new instance of this class.
+	 * 
+	 * @param strict If true, creates a strict parser. Otherwise, creates a lazy one.
+	 */
 	public VersionParser(boolean strict) {
 		this.strict = strict;
 	}
 	
-	public VersionInfo parse(String s) {
+	/**
+	 * Parses a given version string.
+	 * 
+	 * @param s The version to be parsed.
+	 * @return The instance of VersionInfo that contains the version information. 
+	 * @throws IllegalArgumentException If the version string is invalid.
+	 */
+	public VersionInfo parse(String s) throws IllegalArgumentException {
 		VersionInfo v;
 		String parts[];
+		int count;
+		
+		if (s == null) {
+			throw new IllegalArgumentException("The version string cannot be null.");
+		}
 		
 		v = new VersionInfo();
 		parts = s.split("\\-", 2);
-		if (parts.length > 1) {
-			if (strict) {
-				// Try as x.y.z
-				parseMajorMinorRevision(parts[0], v, 3);
-			} else {
-				// Try as x.y.z.b
-				parseMajorMinorRevision(parts[0], v, 4);
+		
+		// Try the first part
+		count = parseMajorMinorRevisionBuild(parts[0], v);
+		if (count < 0) {
+			throw new IllegalArgumentException(String.format("Invalid version string '%1$s'.", s));
+		}
+
+		// Validate the result
+		if (parts.length == 1) {
+			if ((strict) && (count > 3)) {
+				throw new IllegalArgumentException(String.format("Invalid version string '%1$s'.", s));
 			}
 		} else {
 			// Try as x.y.z-qualifier-b
-
-			// Process x.y.z
-			parseMajorMinorRevision(parts[0], v, 3);
+			if (count > 3) {
+				throw new IllegalArgumentException(String.format("Invalid version string '%1$s'.", s));
+			}
 			
 			// Process qualifier-build
-			parseQualifierBuild(parts[1], v);
+			if (!parseQualifierBuild(parts[1], v)) {
+				throw new IllegalArgumentException(String.format("Invalid version string '%1$s'.", s));
+			}
 		}
 		
 		return v;
 	}
 	
-	private void parseMajorMinorRevision(String s, VersionInfo v, int maxParts) {
-		String parts[];
-
-		// Check for empty string
-		if (s.isEmpty()) {
-			throw new IllegalArgumentException("The version is");
-		}
+	/**
+	 * Parses the version string in the format "x.y[.z[.b]]" and sets the major, minor, revision and build
+	 * of the VersionInfo instance v.
+	 * 
+	 * @param s The string to be parsed.
+	 * @param v The version to be filled.
+	 * @return The number of parts found or -1 if the version string is invalid.
+	 */
+	private int parseMajorMinorRevisionBuild(String s, VersionInfo v) {
+		Matcher m;
 		
-		// Split on points
-		parts = s.split("\\.");
-
-		// Check the maximum parts
-		if (parts.length > maxParts) {
-			throw new IllegalArgumentException("Invalid version. Too much parts.");
-		}
 		
-		// Missing part
-		if (parts.length == 1) {
-			throw new IllegalArgumentException("Missing minor.");
-		}
-
-		// Major
-		try {
-			v.setMajor(Integer.parseInt(parts[0]));
-		}catch (NumberFormatException e) {
-			throw new IllegalArgumentException("Major is not a valid number.");
-		}
-
-		// Minor
-		try {
-			v.setMinor(Integer.parseInt(parts[1]));
-		}catch (NumberFormatException e) {
-			throw new IllegalArgumentException("Minor is not a valid number.");
-		}
-
-		// Revision
-		if (parts.length > 2) {
-			try {
-				v.setRevision(Integer.parseInt(parts[2]));
-			}catch (NumberFormatException e) {
-				throw new IllegalArgumentException("Revision is not a valid number.");
+		m = MAJOR_MINOR_REVISION_BUILD_PATTERN.matcher(s);
+		if (m.find()) {
+			// Major
+			v.setMajor(Integer.parseInt(m.group(1)));
+			
+			// Minor
+			v.setMinor(Integer.parseInt(m.group(2)));
+			
+			// Revision
+			if (m.group(3) != null) {
+				v.setRevision(Integer.parseInt(m.group(3)));
+				
+				// Build
+				if (m.group(4) != null) {
+					v.setBuild(Integer.parseInt(m.group(4)));
+					return 4;
+				} else {
+					return 3;
+				}
+			} else {
+				return 2;
 			}
-		}
-		
-		// Build
-		if (parts.length > 3) {
-			try {
-				v.setBuild(Integer.parseInt(parts[3]));
-			}catch (NumberFormatException e) {
-				throw new IllegalArgumentException("Build is not a valid number.");
-			}
+		} else {
+			return -1;			
 		}
 	}
 	
-	private void parseQualifierBuild(String s, VersionInfo v) {
-		String parts[];
-		int build;
-		String qualifier;
+	/**
+	 * Parses the qualifier-build part in the format "[b]", "[q]" or "[q][-b]". 
+	 *  
+	 * @param s The string to be parsed.
+	 * @param v The version to be filled.
+	 * @return True for success of false otherwise.
+	 * @exception IllegalArgumentException If the parameters are invalid.
+	 */
+	private boolean parseQualifierBuild(String s, VersionInfo v) throws IllegalArgumentException{
+		Matcher m;
 		
-		parts = s.split("\\-", 2);
-		// Process part 1
-		try {
-			build = Integer.parseInt(parts[0]);
-			qualifier = null;
-		} catch (NumberFormatException e) {
-			qualifier = parts[0];
-			build = -1;
-		}
-
-		if (parts.length == 1) {
-			// Build or qualifier
-			if (qualifier != null) {
-				v.setQualifier(qualifier);
-			} else {
-				v.setBuild(build);
-			}			
+		// Try build only
+		m = BUILD_PATTERN.matcher(s);
+		if (m.find()) {
+			// Build only
+			v.setBuild(Integer.parseInt(m.group(1)));
+			return true;
 		} else {
-			// qualifier and build
-			if (qualifier == null) {
-				throw new IllegalArgumentException("Invalid qualifier.");
+			// Try qualifier-build
+			m = QUALIFIER_BUILD_PATTERN.matcher(s);
+			if (m.find()) {
+				// Qualifier?
+				if (m.group(1) != null) {
+					v.setQualifier(m.group(1));
+					// build?
+					if (m.group(2) != null) {
+						v.setBuild(Integer.parseInt(m.group(2)));
+					}
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
 			}
-			v.setQualifier(qualifier);
-			
-			try {
-				build = Integer.parseInt(parts[1]);
-			} catch (NumberFormatException e) {
-				throw new IllegalArgumentException("Invalid build number.");				
-			}
-			v.setBuild(build);
 		}
 	}
 }
--- libs/maven-versions-core/src/test/java/br/com/brokenbits/mvn/versions/VersionParserTest.java
+++ libs/maven-versions-core/src/test/java/br/com/brokenbits/mvn/versions/VersionParserTest.java
@@ -7,15 +7,183 @@ import org.junit.Test;
 import br.com.brokenbits.mvn.versions.VersionParser;
 
 public class VersionParserTest {
+	
+	private static final String FAILED_VERSIONS[] = {
+		"",
+		" ",
+		"1",
+		"1.2.3.4.5",
+		".2.3.4",
+		"1..3.4",
+		"1.2..4",
+		"1.2.3.",
+		"a.2.3.4",
+		"1.a.3.4",
+		"1.2.a.4",
+		"1.2.3.a",
+		"1.2.3.4-",
+		"1.2.3-",
+		"1.2-",
+		"1-",
+		"1.2.3-4-qualifier",
+		"1.2.3.4-qualifier",
+		"1.2.3.4-qualifier-qualifier",		
+	};
 
+	private static final String FAILED_STRICT_VERSIONS[] = {
+		"",
+		" ",
+		"1",
+		"1.2.3.4",
+		".2.3",
+		"1..3",
+		"1.2.",		
+		"a.2.3",
+		"1.a.3",
+		"1.2.a",
+		"1.2.3.4-",
+		"1.2.3-",
+		"1.2-",
+		"1-",
+		"1.2.3-4-qualifier",
+		"1.2.3.4-qualifier",
+		"1.2.3.4-qualifier-qualifier"
+	};	
+	
 	@Test
 	public void testParse() {
 		VersionParser p;
+		VersionInfo v;
 		
 		p = new VersionParser(false);
 		
-		p.parse("1.2");
+		v = p.parse("1.2");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(0, v.getRevision());
+		assertEquals(0, v.getBuild());
+		assertNull(v.getQualifier());
 		
+		v = p.parse("1.2.3");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(3, v.getRevision());
+		assertEquals(0, v.getBuild());
+		assertNull(v.getQualifier());		
+		
+		v = p.parse("1.2.3.4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(3, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertNull(v.getQualifier());
+		
+		v = p.parse("1.2-4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(0, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertNull(v.getQualifier());
+
+		v = p.parse("1.2.3-4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(3, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertNull(v.getQualifier());
+		
+		v = p.parse("1.2-q-4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(0, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertEquals("q", v.getQualifier());
+
+		v = p.parse("1.2.3-q-4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(3, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertEquals("q", v.getQualifier());
+	}
+	
+	@Test
+	public void testParseFailed() {
+		VersionParser p;
+		VersionInfo v;
+		
+		p = new VersionParser(false);
+		for (String s: FAILED_VERSIONS) {
+			try {
+				v = p.parse(s);
+				fail(String.format("Version '%1$s' should be invalid.", s));
+				assertNull(v);
+			} catch (IllegalArgumentException e){}
+		}
 	}
 
+	@Test
+	public void testParseStrict() {
+		VersionParser p;
+		VersionInfo v;
+		
+		p = new VersionParser(true);
+		
+		v = p.parse("1.2");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(0, v.getRevision());
+		assertEquals(0, v.getBuild());
+		assertNull(v.getQualifier());
+		
+		v = p.parse("1.2.3");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(3, v.getRevision());
+		assertEquals(0, v.getBuild());
+		assertNull(v.getQualifier());
+		
+		v = p.parse("1.2-4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(0, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertNull(v.getQualifier());
+
+		v = p.parse("1.2.3-4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(3, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertNull(v.getQualifier());
+		
+		v = p.parse("1.2-q-4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(0, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertEquals("q", v.getQualifier());
+
+		v = p.parse("1.2.3-q-4");
+		assertEquals(1, v.getMajor());
+		assertEquals(2, v.getMinor());
+		assertEquals(3, v.getRevision());
+		assertEquals(4, v.getBuild());
+		assertEquals("q", v.getQualifier());		
+	}
+	
+	@Test
+	public void testParseStrictFailed() {
+		VersionParser p;
+		VersionInfo v;
+		
+		p = new VersionParser(true);
+		for (String s: FAILED_STRICT_VERSIONS) {
+			try {
+				v = p.parse(s);
+				fail(String.format("Version '%1$s' should be invalid.", s));
+				assertNull(v);
+			} catch (IllegalArgumentException e){}
+		}
+	}
 }
